b78e505a20bb9b978851962371fd97ba145cebfc
BrennerHumberto <brener.aioros@gmail.com>
2014-06-30 08:12:06 -0500

    Creacion de los POJOS Libro y Usuario

---
+++ SistemaBiblioteca/src/db/Libro.java
@@ -0,0 +1,25 @@
+package db;
+
+public class Libro {
+	private String codigoLibro;
+	private String titulo;
+	private String Autor;
+	public String getCodigoLibro() {
+		return codigoLibro;
+	}
+	public void setCodigoLibro(String codigoLibro) {
+		this.codigoLibro = codigoLibro;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return Autor;
+	}
+	public void setAutor(String autor) {
+		Autor = autor;
+	}
+}
---
+++ SistemaBiblioteca/src/db/Usuario.java
@@ -0,0 +1,53 @@
+package db;
+
+public class Usuario {
+	private String DNI;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String email;
+    private String Password;
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+}
